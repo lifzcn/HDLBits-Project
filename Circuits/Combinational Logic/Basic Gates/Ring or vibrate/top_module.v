b8718c1f96d39ae2bbbad1d6a2e71adc246e0a71
LiFZ <lifzcn@qq.com>
2024-02-29 22:08:21 +0800

    Update some files

--- Circuits/Combinational Logic/Basic Gates/Ring or vibrate/top_module.v
+++ Circuits/Combinational Logic/Basic Gates/Ring or vibrate/top_module.v
@@ -1,10 +1,25 @@
-module top_module (
+module top_module(
     input ring,
     input vibrate_mode,
     output ringer,
     output motor
 );
 
+    always @(ring) begin
+        if (ring == 1'b0) begin
+            ringer = 1'b0;
+            motor = 1'b0;
+        end
+        else begin
+            if (vibrate_mode == 0) begin
+                ringer = 1'b1;
+                motor = 1'b0;
+            end
+            else begin
+                ringer = 1'b0;
+                motor = 1'b1;
+            end
+        end
+    end
     
-
 endmodule
